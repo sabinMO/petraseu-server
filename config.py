8afdfe76854a5c07f4df2da45d1378ac2979fbe9
sabinMO <mariansabin46@yahoo.com>
2026-09-06 12:17:05 +0300

    Update Graphhopper API key in config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-GRAPHHOPPER_API_KEY = "cheia_ta"
+GRAPHHOPPER_API_KEY = "cheia_ta_graphhopper"
 GITHUB_TOKEN = "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"
 GIST_ID = "d3f55e3c956adbca38dd0b8ed0cb4a8b"
 DATABASE_FILE = "database.json"
